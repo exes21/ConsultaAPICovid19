b87676277aed9c8b0b1857b90715aeee7b379086
exes21 <32020278+exes21@users.noreply.github.com>
2022-08-26 11:48:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+window.onload = iniciar();
+
+function iniciar() {
+
+    let btnBuscar = document.getElementById("btnBuscarDatos");
+
+    btnBuscar.addEventListener("click", clicKBoton);
+
+
+}
+
+async function cargarURL(url) {
+    let response = await fetch(url);
+    return response.json();
+}
+
+async function clicKBoton() {
+    let json = await cargarURL("https://api.covidtracking.com/v1/us/daily.json");
+    let Inputfecha = document.getElementById("inputFecha").value;
+    let stringFecha = Inputfecha.slice(0, 4) + Inputfecha.slice(5, 7) + Inputfecha.slice(8, 10);
+    let positivos = document.getElementById("positivos");
+    let negativos = document.getElementById("negativos");
+    let hospitalizados = document.getElementById("hospitalizados");
+
+    if (stringFecha > "20210307" || stringFecha < "20200113") {
+        alert("No contamos con datos de esa fecha, la última actualización de datos fue el 07 de marzo del 2021" +
+            "el primer dato registrado fue el 13 de enero del 2020");
+
+    }
+    else {
+        
+        for (let i = 0; i < json.length; i++) {
+
+            if (json[i].date == stringFecha) {
+                positivos.innerText = json[i].positive;
+                negativos.innerText = json[i].negative;
+                hospitalizados.innerText = json[i].hospitalized;
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+}
